30586110b648c2eeaed34300b4b0a19e2476fdb2
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2024-01-28 13:48:32 +0530

    Add files via upload

---
+++ 01-variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "Kushagra.chaturvedi.bhargava@gmail.com"
+let accountPassword = "Kushagra12345"
+accountCity = "Lucknow"
+let accountState
+
+
+
+//accountPassword = "123454" allowed and response accepted
+//accountEmail = "Eahiu@gmail.com" allowed and response accepted
+//accountId = 2  not allowed
+//accountCity = "Jaipur" allowed and response accepted
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// Curly braces {} is called scope.
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
